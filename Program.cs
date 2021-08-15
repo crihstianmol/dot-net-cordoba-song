f5aae89a8a4c17e4730d333282b9d1efd0736e3e
Crihstian Camilo Molina <cristiancm1235@gmail.com>
2021-08-14 19:28:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace dot_net_cordoba_song
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep(493,1000);//SI
+            Console.Beep(392,500);//SOL
+            Console.Beep(493,1000);//SI
+            Console.Beep(392,500);//SOL
+            Console.Beep(493,400);//SI
+            Console.Beep(440,400);//LA
+            Console.Beep(392,400);//SOL
+            Console.Beep(440,1000);//LA
+            Console.Beep(392,1000);//SOL
+            Console.Beep(329,500);//MI
+            Console.Beep(392,1000);//SOL
+            Console.Beep(329,500);//MI
+            Console.Beep(493,400);//SI
+            Console.Beep(440,400);//LA
+            Console.Beep(392,400);//SOL
+            Console.Beep(440,1000);//LA
+            Console.Beep(493,1000);//SI
+            Console.Beep(587,400);//RE
+            Console.Beep(493,400);//SI
+            Console.Beep(493,400);//SI
+            Console.Beep(440,400);//LA
+            Console.Beep(392,400);//SOL
+            Console.Beep(493,400);//SI
+            Console.Beep(440,400);//LA
+            Console.Beep(392,400);//SOL
+            Console.Beep(440,1000);//LA
+            Console.Beep(392,400);//SOL
+            Console.Beep(392,400);//SOL
+            Console.Beep(440,400);//LA
+            Console.Beep(493,400);//SI
+            Console.Beep(523,400);//DO
+            Console.Beep(493,400);//SI
+            Console.Beep(440,400);//LA
+            Console.Beep(392,1000);//SOL
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Tocar una canción en C# 
+## En este proyecto, se encuentran las notas base del Himno a la Institución Educativa Ciudad Córdoba
+## Esa institución me formó como persona
+
+# Play a song in C#
+## In this project, are the base notes of Institución Educativa Ciudad Córdoba hymn 
+## That institution formed me as a person
